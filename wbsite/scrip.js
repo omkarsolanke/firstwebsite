d61b80343ec52406e7b7e0fd1682fa8f73e88f70
Omkar Solanke <110219809+omkarsolanke@users.noreply.github.com>
2022-09-12 14:50:36 +0530

    Add files via upload

---
+++ wbsite/scrip.js
@@ -0,0 +1,10 @@
+let na = document.querySelector(".show");
+na.addEventListener('click',showMsg);
+
+
+function showMsg(){
+    alert("Thank You ....For Submit....");
+}
+function Submit(){
+    console.log("thank you");
+}
